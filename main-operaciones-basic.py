564a20943761d5fa372215069c79368b209aa92d
CARLOS SORIA <88163507+Carlositoskun@users.noreply.github.com>
2023-03-21 18:52:52 -0500

    Add files via upload

---
+++ main-operaciones-basic.py
@@ -0,0 +1,10 @@
+import operaciones
+
+s= operaciones.suma(2, 1)
+print("el resultado es: ",s)
+r= operaciones.resta(6, 1)
+print("el resultado es: ",r)
+mul= operaciones.resta(6, 2)
+print("el resultado es: ",mul)
+divo= operaciones.resta(6, 3)
+print("el resultado es: ",divo)
